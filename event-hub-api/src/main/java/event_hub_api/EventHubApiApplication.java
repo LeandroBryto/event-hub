8a1e3c73b9c1fc1ce45541c2c8c65072d8312854
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2026-02-01 11:43:48 -0300

    feat(backend): estrutura inicial do projeto spring boot

---
+++ event-hub-api/src/main/java/event_hub_api/EventHubApiApplication.java
@@ -0,0 +1,13 @@
+package event_hub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventHubApiApplication.class, args);
+	}
+
+}
---
+++ event-hub-api/src/test/java/event_hub_api/EventHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package event_hub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
